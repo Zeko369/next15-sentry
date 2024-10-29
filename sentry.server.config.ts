1328485b1e53b88b60055258b7f82eda37a83e4c
Fran Zekan <zekan.fran369@gmail.com>
2024-10-29 11:23:42 +0100

    Broken dynamicIO

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://18c80c12e53847adaf451d919c5eb283@o416355.ingest.us.sentry.io/4504355037642752",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
